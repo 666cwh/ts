791be70e96939bcf25e7fd0fd1166c205eb9db26
chengweihua <15133266546@sohu.com>
2021-03-25 22:56:53 +0800

    fixL测试

--- demo6.ts
+++ demo6.ts
@@ -2,19 +2,20 @@
  * 抽象类
  */
 
-
 //readonly:设置只读属性
-class ClassAbstract{
-  public readonly _name:string = ''   
-  constructor(name:string){
-    this._name = name
+class ClassAbstract {
+  public readonly _name: string = '';
+  constructor(name: string) {
+    this._name = name;
   }
-  get name(){
-    return this._name
+  get name() {
+    return this._name;
   }
 }
 
 const classAbstract = new ClassAbstract('cheng');
-classAbstract.name = 'hello'  //无法分配到 "name" ，因为它是只读属性。
+classAbstract.name = 'hello'; //无法分配到 "name" ，因为它是只读属性。
 
-console.log(classAbstract.name)
+console.log(classAbstract.name);
+
+123;
