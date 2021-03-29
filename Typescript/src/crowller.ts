377c5a633327c3e1b5100d172c2a7490ef573f47
cwh <wb_hd02@dahuatech.com>
2021-03-29 14:52:21 +0800

    fix:爬虫编写

--- Typescript/src/crowller.ts
+++ Typescript/src/crowller.ts
@@ -1,18 +1,72 @@
-// http://www.dell-lee.com/typescript/demo.html?secret=x3b174jsx
+// 爬虫地址:http://www.dell-lee.com/typescript/demo.html?secret=x3b174jsx
 
 import superagent from 'superagent';  //引入爬虫工具
+import cheerio from 'cheerio' //引入cheerio
+import fs from 'fs'
+import path from 'path'
+
+interface Course{
+  title:string;
+  count:number
+}
+interface CoursResult{
+  time:number,
+  data:Course[]
+}
+interface Centent{
+  [propName:number] : Course[]
+}
 class Crowller {
   //private:属性只允许在类内使用
   private secret = 'x3b174jsx';  
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`
-  private reptileData = ''
+  //格式化数据
+  getJson(html:string){
+    const $ = cheerio.load(html)
+    const courItem = $('.course-item')
+    const arr:Course[] = []
+    // console.log(courItem.length)
+    courItem.map((index,element)=>{
+      const desc = $(element).find('.course-desc')
+      const title = desc.eq(0).text()
+      const count = +desc.eq(1).text().split('：')[1]
+      console.log(title,count)
+      arr.push({
+        title,count
+      })
+    })
+    return {
+      time:(new Date()).getTime(),
+      data:arr
+    }
+  }
+   //从远程地址抓取html内容
   async reptileWay(){
     const beg = await superagent.get(this.url)  
-    this.reptileData = beg.text   //从远程地址获取html内容
-    console.log(this.reptileData)
+    return beg.text
+  }
+  //存储json
+  courseInfor(coursInfor:CoursResult){
+    const filePath = path.resolve(__dirname,'../data/coures.json')
+    let content:Centent = {}
+    //判断文件路径是否存在
+    if(fs.existsSync(filePath)){
+      content = JSON.parse( fs.readFileSync(filePath,'utf-8'))
+    }
+    content[coursInfor.time] =coursInfor.data
+    return content
+  }
+  // 获取数据
+  async operationa(){
+    const filePath = path.resolve(__dirname,'../data/coures.json')
+    const html =await this.reptileWay()
+    const coursInfor = this.getJson(html)
+    const fileContent = this.courseInfor(coursInfor)
+    fs.writeFileSync(filePath,JSON.stringify(fileContent))
+    console.log(coursInfor)
   }
   constructor(){
-    this.reptileWay()
+    this.operationa()
   }
 }
 
