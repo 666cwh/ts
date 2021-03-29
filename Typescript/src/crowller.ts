802b24961ab8eaefcc3e7bfb156af47f8a028135
cwh <wb_hd02@dahuatech.com>
2021-03-29 10:43:34 +0800

    fix:获取远程dom

--- Typescript/src/crowller.ts
+++ Typescript/src/crowller.ts
@@ -1,1 +1,21 @@
+// http://www.dell-lee.com/typescript/demo.html?secret=x3b174jsx
+
+import superagent from 'superagent';  //引入爬虫工具
+class Crowller {
+  //private:属性只允许在类内使用
+  private secret = 'x3b174jsx';  
+  private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`
+  private reptileData = ''
+  async reptileWay(){
+    const beg = await superagent.get(this.url)  
+    this.reptileData = beg.text   //从远程地址获取html内容
+    console.log(this.reptileData)
+  }
+  constructor(){
+    this.reptileWay()
+  }
+}
+
+const crowller = new Crowller()
+
 
